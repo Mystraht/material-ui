75150dc43d7b07fe093d2cb1d6156dc40b121936
Your Name <you@example.com>
2019-12-01 18:26:10 +0100

    test

--- scripts/set-scopes.js
+++ scripts/set-scopes.js
@@ -11,7 +11,7 @@ exec('git diff --name-only origin/master', (error, stdout) => {
   modifiedFiles
     .forEach((line) => {
       if (line.match(/jsx|js/)) {
-        setEnvVariable('JS_MODIFIED', 'true');
+        // setEnvVariable('JS_MODIFIED', 'true');
         console.log(line);
       }
     });
